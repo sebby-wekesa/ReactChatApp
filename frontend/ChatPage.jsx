2ed71b92e4517034aefbcb3839462d67cfccf8ab
sebby-wekesa <sebbywakis@gmail.com>
2025-08-24 14:07:07 +0300

    add license.txt file

--- frontend/ChatPage.jsx
+++ frontend/ChatPage.jsx
@@ -1,16 +1,17 @@
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatPage = (props) => {
-    return(
-        <div className="background">
-            <PrettyChatWindow
-            const projectId = import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID;
-            username = (props.user.Username)
-            secret = (props.user.secret)
-            />
-        </div>
+  const projectId = import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID;
 
-    );
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={projectId}
+        username={props.user.username} // Fixed casing (Username -> username)
+        secret={props.user.secret}
+      />
+    </div>
+  );
 };
 
 export default ChatPage;
